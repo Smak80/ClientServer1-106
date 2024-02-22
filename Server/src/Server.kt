e30302f692d24efddf6bebe13a41afb46006ea7b
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-22 13:40:28 +0300

    Серверная часть

--- Server/src/Server.kt
+++ Server/src/Server.kt
@@ -1,2 +1,31 @@
-class Server {
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+
+class Server(var port: Int = 5106) {
+
+    private val serverSocket = ServerSocket(port)
+
+    fun start(){
+        var clientSocket: Socket? = null
+        try {
+            clientSocket = serverSocket.accept()
+            with(BufferedReader(InputStreamReader(clientSocket.getInputStream()))) {
+                val clientMessage = readLine()
+                println(clientMessage)
+            }
+
+            with(PrintWriter(clientSocket.getOutputStream())) {
+                println("Привет от сервера!")
+                flush()
+            }
+        }
+        catch (_: Throwable){
+            println("Ошибка ;(")
+        } finally {
+            clientSocket?.close()
+        }
+    }
 }
